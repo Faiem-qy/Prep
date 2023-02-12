15578558f3ffb9b9b5b930e3a2bbddcac62ae39a
Faiem Quan Young <fquanyoung@gmail.com>
2023-02-11 22:43:58 -0500

    .

--- lighthouse-js-fundamentals/conditionals.js
+++ lighthouse-js-fundamentals/conditionals.js
@@ -36,16 +36,28 @@ console.log("Now you're ready to go outside!");
 
 // Logical Operators
 
-const isCitizen = true;
-const age = 26;
-if (isCitizen && age > 18) {
-  console.log("You are eligible to vote");
-}
+// const isCitizen = true;
+// const age = 26;
+// if (isCitizen && age > 18) {
+//   console.log("You are eligible to vote");
+// }
 
-if (temperature < -40 || temperature > 40) {
-  console.log("Maybe going outside isn't such a good idea");
-}
+// if (temperature < -40 || temperature > 40) {
+//   console.log("Maybe going outside isn't such a good idea");
+// }
 
-if (!raining) {
-  console.log("Leave your umbrella at home!");
-}
+// if (!raining) {
+//   console.log("Leave your umbrella at home!");
+// }
+
+const whichSchool = function (age) {
+  if (age < 13) {
+    return "Elementary";
+  } else if (age >= 13 && age <= 18) {
+    return "Secondary School";
+  } else {
+    return "Lighthouse Labs";
+  }
+};
+
+whichSchool(1);
