1908fc9ad759c700c926e2193f7579e1c9691353
Faiem-qy <fquanyoung@gmail.com>
2023-02-12 22:33:36 -0500

    functions

---
+++ lighthouse-js-fundamentals/even.js
@@ -0,0 +1,12 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+};
+
+// const tenIsEven = isEven(10);// not necssary
+// const elevenIsEven = isEven(11);// not necessary
+
+// console.log(tenIsEven);
+// console.log(elevenIsEven);
+
+console.log(isEven(10));
+console.log(isEven(11));
