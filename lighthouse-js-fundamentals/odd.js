1a060b9e126196fdd8078a02e0afda5804baa713
Faiem-qy <f.quanyoung@gmail.com>
2023-02-16 22:59:14 -0500

    odd.js

--- lighthouse-js-fundamentals/odd.js
+++ lighthouse-js-fundamentals/odd.js
@@ -8,3 +8,28 @@ function isOdd(number) {
 
 console.log("3 is odd: " + isOdd(3));
 console.log("8 is odd: " + isOdd(8));
+
+const stations = [
+  ["Big Bear Donair", 10, "restaurant"],
+  ["Bright Lights Elementary", 50, "school"],
+  ["Moose Mountain Community Centre", 45, "community centre"],
+];
+
+const chooseStations = function (stations) {
+  // Your code in here ...
+  const goodStations = [];
+  for (const station of stations) {
+    const capacity = station[1];
+
+    if (capacity >= 20) {
+      const type = station[2];
+
+      if (type === "school" || type === "community centre") {
+        goodStations.push(station[0]);
+      }
+    }
+  }
+  return goodStations;
+};
+
+console.log(chooseStations(stations));
