918b39ad81f2ed85cdd0eb9946075b050d9939ec
Faiem-qy <f.quanyoung@gmail.com>
2023-02-17 22:56:01 -0500

    odd.js

--- lighthouse-js-fundamentals/odd.js
+++ lighthouse-js-fundamentals/odd.js
@@ -1,14 +1,15 @@
-function isOdd(number) {
-  if (number % 2 == 0) {
-    return false;
-  } else {
-    return true;
-  }
-}
+// function isOdd(number) {
+//   if (number % 2 == 0) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
 
-console.log("3 is odd: " + isOdd(3));
-console.log("8 is odd: " + isOdd(8));
+// console.log("3 is odd: " + isOdd(3));
+// console.log("8 is odd: " + isOdd(8));
 
+/*
 const stations = [
   ["Big Bear Donair", 10, "restaurant"],
   ["Bright Lights Elementary", 50, "school"],
@@ -33,3 +34,27 @@ const chooseStations = function (stations) {
 };
 
 console.log(chooseStations(stations));
+
+*/
+// x marks the perfect shot
+const moves = ["north", "north", "west", "west", "north", "east", "north"];
+
+function finalPosition(moves) {
+  let x = 0;
+  let y = 0;
+
+  for (let i = 0; i < moves.length; i++) {
+    if (moves[i] === "north") {
+      y++;
+    } else if (moves[i] === "south") {
+      y--;
+    } else if (moves[i] === "east") {
+      x++;
+    } else if (moves[i] === "west") {
+      x--;
+    }
+  }
+  return [x, y];
+}
+
+console.log(finalPosition(moves));
