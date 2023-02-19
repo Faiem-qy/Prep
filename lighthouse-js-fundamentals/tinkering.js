1cd58f9c337401f082ed33a36260e8086b774296
Faiem-qy <f.quanyoung@gmail.com>
2023-02-18 22:56:14 -0500

    tinkering

--- lighthouse-js-fundamentals/tinkering.js
+++ lighthouse-js-fundamentals/tinkering.js
@@ -5,3 +5,12 @@ function multiply(a, b) {
 const result = multiply(2, 4);
 
 console.log(result);
+
+function ageCalculator(name, yearOfBirth, currnetYear) {
+  const age = currnetYear - yearOfBirth;
+
+  return `${name} is ${age} years old.`;
+}
+console.log(ageCalculator("Suzie", 1989, 2015));
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
