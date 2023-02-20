6404eca49ea63d0f301745bd33e8ae3e757f525c
Faiem-qy <f.quanyoung@gmail.com>
2023-02-19 23:41:02 -0500

    tinkering

--- lighthouse-js-fundamentals/tinkering.js
+++ lighthouse-js-fundamentals/tinkering.js
@@ -1,3 +1,4 @@
+/*
 function multiply(a, b) {
   return a * b;
 }
@@ -14,3 +15,23 @@ function ageCalculator(name, yearOfBirth, currnetYear) {
 console.log(ageCalculator("Suzie", 1989, 2015));
 console.log(ageCalculator("Miranda", 1983, 2015));
 console.log(ageCalculator("Ferdinand", 1988, 2015));
+
+*/
+
+let number = 987654321;
+function howManyHundreds(number) {
+  if (number < 100) {
+    return `There are ${0} hundreds`;
+  } else if (number >= 100 && number < 1000) {
+    return `There are ${Math.trunc(number / 100)} hundreds`;
+  } else if (number >= 1000) {
+    return `There are ${Math.trunc(number / 100)} hundreds`;
+  }
+}
+console.log(howManyHundreds(number));
+
+console.log(howManyHundreds(1000));
+console.log(howManyHundreds(894));
+console.log(howManyHundreds(520));
+console.log(howManyHundreds(99));
+console.log(howManyHundreds(0));
