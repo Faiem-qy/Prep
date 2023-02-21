55771418b80968bdd499aae4a67281167f94ffa5
Faiem-qy <f.quanyoung@gmail.com>
2023-02-20 23:33:17 -0500

    array.js

---
+++ lighthouse-js-fundamentals/array.js
@@ -0,0 +1,54 @@
+"use strict";
+
+/*
+const director = "Steven Spielberg";
+const films = ["Jaws", "Raiders of the Lost Ark", "A.I."];
+
+console.log("My favourite " + director + " film is " + films[2]);
+
+films[2] = "Jurassic Park";
+films;
+films.push("A.I.");
+films;
+
+console.log("My favourite " + director + " film is " + films[2]); // after mutation
+
+console.log("films at index 0: " + films[0]);
+console.log("films at index 1: " + films[1]);
+console.log("films at index 2: " + films[2]);
+console.log("films at index 3: " + films[3]);
+
+// const oddNumbers = [1, 3, 5, 7, 9];
+// const randomStuff = [1.15, "bicycle", true];
+
+console.log(director + " is the director of " + films.length + " films");
+
+films.push("E.T.");
+console.log("films at index 4: " + films[4]);
+console.log(director + " is the director of " + films.length + " films");
+
+*/
+
+//arrays ice cream!
+
+const iceCreamFlavours = [
+  "chocolate",
+  "vanilla",
+  "cookies and cream",
+  "rocky road",
+  "strawberry",
+];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);
+
+let a = 3;
+let b = 4;
+b = a;
+console.log(b);
+
+const number = 2 + 2;
