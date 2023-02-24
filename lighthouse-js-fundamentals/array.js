f042360a18f3ec0dcd2dd30518d459ff82ffda6f
Faiem-qy <f.quanyoung@gmail.com>
2023-02-23 21:50:39 -0500

    array

--- lighthouse-js-fundamentals/array.js
+++ lighthouse-js-fundamentals/array.js
@@ -48,8 +48,65 @@ console.log(iceCreamFlavours[5]);
 console.log(iceCreamFlavours.length);
 
 */
-const packingList = ["tent", "bug spray", "sleeping bag"];
+// const packingList = ["tent", "bug spray", "sleeping bag"];
 
-console.log(packingList[0]);
-console.log(packingList[1]);
-console.log(packingList[2]);
+// console.log(packingList[0]);
+// console.log(packingList[1]);
+// console.log(packingList[2]);
+
+/*
+//Range - loop
+const range = function (start, end, step) {
+  let array = [];
+  if (start === undefined || end === undefined || step === undefined) {
+    return array;
+  } else if (start > end) {
+    return array;
+  } else if (step <= 0) {
+    return array;
+  } else {
+    for (let i = start; i <= end; i += step) {
+      array.push(i);
+    }
+  }
+  return array;
+};
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+
+console.log(range(5, 12, undefined));
+console.log(range(5, 2, 3));
+console.log(range(-5, 2, 0));
+
+*/
+
+/*
+// lastindexof
+
+const lastIndexOf = function (arr, val) {
+  return val ? arr.lastIndexOf(val) : -1;
+};
+console.log(lastIndexOf([0, 1, 4, 1, 2], 1));
+console.log(lastIndexOf([0, 1, 4, 1, 2], 2));
+console.log(lastIndexOf([0, 1, 4, 1, 2], 3));
+console.log(lastIndexOf([5, 5, 5], 5));
+console.log(lastIndexOf([], 3));
+
+*/
+
+// concatenate arrays
+/*
+const concat = (a, b) => {
+  for (let i = 0; i < b.length; i++) {
+    a.push(b[i]);
+  }
+  return a;
+};
+
+console.log(concat([1, 2, 3], [4, 5, 6]));
+console.log(concat([0, 3, 1], [9, 7, 2]));
+console.log(concat([], [9, 7, 2]));
+console.log(concat([5, 10], []));
+*/
