02c54de65a015696b8e9529b0f8cf29cf3616787
Faiem-qy <f.quanyoung@gmail.com>
2023-02-23 22:09:27 -0500

    index

--- lighthouse-js-fundamentals/ingredients.js
+++ lighthouse-js-fundamentals/ingredients.js
@@ -1,3 +1,5 @@
+"use strict";
+/*
 const ingredients = [
   "eggs",
   "milk",
@@ -25,3 +27,47 @@ console.log("---Reverse Loop---");
 for (let i = ingredients.length - 1; i >= 0; i--) {
   console.log(ingredients[i]);
 }
+*/
+
+// function range(start, end, step) {
+//   const array = [];
+//   for (let i = start; i <= end; i + step) {
+//     console.log("i - ", i);
+//     array.push(i);
+//     console.log("array", array);
+//   }
+// }
+
+/*
+//Range - loop
+const range = function (start, end, step) {
+  let array = [];
+  if (start === undefined || end === undefined || step === undefined) {
+    console.log([]);
+  } else if (start > end) {
+    console.log([]);
+  } else if (step <= 0) {
+    console.log([]);
+  }
+  for (i = start; i <= end; i += step) {
+    array.push(i);
+  }
+  return array;
+};
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+
+console.log(range(5, 2, 3));
+console.log(range(5, 2, 3));
+console.log(range(-5, 2, 0));
+
+
+// lastindexof
+
+// const l = function (lastindexof)
+*/
+
+// concatenate arrays
+
