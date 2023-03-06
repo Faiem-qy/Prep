dfc5469f4eedf806ea9c8496f4ca73c9bc937e80
Faiem-qy <f.quanyoung@gmail.com>
2023-03-05 23:15:31 -0500

    elements

---
+++ lighthouse-js-fundamentals/elements.js
@@ -0,0 +1,17 @@
+{
+  /* <p id="intro">
+  Hello World!
+</p>
+
+<p id="info">
+  JavaScript on The DOM
+</p> */
+}
+
+const paragraphs = document.getElementsByTagName("p");
+alert(paragraphs[0].innerHTML);
+alert(paragraphs[1].innerHTML);
+
+/* console.log(paragraphs[0,1].innerHTML) */
+
+// HTMLCollection
