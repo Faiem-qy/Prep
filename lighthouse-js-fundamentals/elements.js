a92c0cc23bb165d36c1f20bbc4d4f40d42546abf
Faiem-qy <f.quanyoung@gmail.com>
2023-03-06 20:43:15 -0500

    elements-class

--- lighthouse-js-fundamentals/elements.js
+++ lighthouse-js-fundamentals/elements.js
@@ -23,3 +23,40 @@ for (let i = 0; i < elm.length; i++) {
   arr.push(elm[i].innerHTML);
 }
 console.log(arr);
+
+// get elements by name
+/*
+<h2>
+  List of People:
+</h2>
+
+
+// HTML
+<ul class='people'>
+  <li class='name'>
+    Clara
+  </li>
+  <li class='name'>
+    James
+  </li>
+  <li class='name'>
+    Sara
+  </li>
+</ul>
+*/
+
+// 1. Create an empty array, `names`
+// 2. Find all the paragraph elements using `getElementsByClassName`
+// 3. For each element found
+// Add the element's text to the array of names
+// 4. Alert the final result
+
+/*
+let names = [];
+let p = document.getElementsByClassName('name');
+
+for (let i = 0;i<p.length; i++){
+names.push(p[i].innerHTML)
+}
+console.log(names)
+*/
