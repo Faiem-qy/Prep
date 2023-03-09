9fdf787ccd50a7be30b0031a103c031c863a168b
Faiem-qy <f.quanyoung@gmail.com>
2023-03-08 21:17:31 -0500

    katas

--- lighthouse-js-fundamentals/katas.js
+++ lighthouse-js-fundamentals/katas.js
@@ -1,7 +1,18 @@
-const sumLargestNumbers = function () {
-  for (let i = 0; i < sumLargestNumbers.length; i++)
-    console.log(sumLargestNumbers);
-};
+function sumLargestNumbers(arr) {
+  let lNum1 = 0;
+  let lNum2 = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > lNum1) {
+      lNum2 = lNum1;
+      lNum1 = arr[i];
+    } else if (arr[i] > lNum2) {
+      lNum2 = arr[i];
+    }
+  }
+
+  return lNum1 + lNum2;
+}
 
 console.log(sumLargestNumbers([1, 10]));
 console.log(sumLargestNumbers([1, 2, 3]));
