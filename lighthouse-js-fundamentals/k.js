471ee8a21077a2f0c282157325008edc749a7ffd
Faiem-qy <f.quanyoung@gmail.com>
2023-03-09 21:10:54 -0500

    K

--- lighthouse-js-fundamentals/k.js
+++ lighthouse-js-fundamentals/k.js
@@ -1,3 +1,5 @@
+// K1
+
 /*
 unction sumLargestNumbers(arr) {
   let lNum1 = 0;
@@ -19,3 +21,22 @@ console.log(sumLargestNumbers([1, 10]));
 console.log(sumLargestNumbers([1, 2, 3]));
 console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
 */
+
+// k2
+
+const conditionalSum = function (values, condition) {
+  let total = 0;
+  for (let i = 0; i < values.length; i++)
+    if (
+      (condition === "even" && values[i] % 2 === 0) ||
+      (condition === "odd" && values[i] % 2 !== 0)
+    ) {
+      total += values[i];
+    }
+  return total;
+};
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
