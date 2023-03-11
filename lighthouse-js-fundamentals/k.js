f71131217854645ea6b5aa1682dd9ee48e5cd001
Faiem-qy <f.quanyoung@gmail.com>
2023-03-10 22:16:26 -0500

    k

--- lighthouse-js-fundamentals/k.js
+++ lighthouse-js-fundamentals/k.js
@@ -22,6 +22,7 @@ console.log(sumLargestNumbers([1, 2, 3]));
 console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
 */
 
+/*
 // k2
 
 const conditionalSum = function (values, condition) {
@@ -40,7 +41,8 @@ console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
 console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
 console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
 console.log(conditionalSum([], "odd"));
-
+*/
+/*
 // K3
 const numberOfVowels = function (data) {
   let vowels = ["a", "e", "i", "o", "u"];
@@ -59,3 +61,77 @@ const numberOfVowels = function (data) {
 console.log(numberOfVowels("orange"));
 console.log(numberOfVowels("lighthouse labs"));
 console.log(numberOfVowels("aeiou"));
+
+// alternate
+const numberOfVowels = function (data) {
+  let count = 0;
+  const vowels = ["a", "e", "i", "o", "u"];
+  for (let i = 0; i < data.length; i++) {
+    if (vowels.includes(data[i].toLowerCase())) {
+      count++;
+    }
+  }
+  return count;
+};
+*/
+
+// k4 Instructors names
+
+// const instructorWithLongestName = function (instructors) {
+//   let length = "";
+//   let instructorsAnswer;
+//   //loop through names
+//   for (let i = 0; i < instructors.length; i++) {
+//     //compare lengths
+//     if (instructors[i].name.length > length) {
+//       length = instructors[i].name.length;
+//       // console.log(length);
+//       instructorsAnswer = instructors[i];
+//       // console.log(instructorsAnswer);
+//     }
+//   }
+//   return instructorsAnswer;
+// };
+
+// console.log(
+//   instructorWithLongestName([
+//     { name: "Samuel", course: "iOS" },
+//     { name: "Jeremiah", course: "Web" },
+//     { name: "Ophilia", course: "Web" },
+//     { name: "Donald", course: "Web" },
+//   ])
+// );
+// console.log(
+//   instructorWithLongestName([
+//     { name: "Matthew", course: "Web" },
+//     { name: "David", course: "iOS" },
+//     { name: "Domascus", course: "Web" },
+//   ])
+// );
+
+const instructorWithLongestName = function (instructors) {
+  return instructors.reduce((prev, current) => {
+    if (current.name.length > prev.name.length) {
+      return current;
+    } else {
+      return prev;
+    }
+  });
+};
+
+console.log(
+  instructorWithLongestName([
+    { name: "Samuel", course: "iOS" },
+    { name: "Ophilia", course: "Web" },
+    { name: "Donald", course: "Web" },
+    { name: "Jeremiah", course: "Web" },
+  ])
+); // {name: "Jeremiah", course: "Web"}
+
+console.log(
+  instructorWithLongestName([
+    { name: "Matthew", course: "Web" },
+    { name: "David", course: "iOS" },
+    { name: "Domascus", course: "Web" },
+  ])
+); // {name: "Domascus", course: "Web"}
