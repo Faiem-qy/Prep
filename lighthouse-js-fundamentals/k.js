0fc690f5e7569abcacd1c09b25ab7246553f44cb
Faiem-qy <f.quanyoung@gmail.com>
2023-03-17 21:18:45 -0400

    k

--- lighthouse-js-fundamentals/k.js
+++ lighthouse-js-fundamentals/k.js
@@ -421,3 +421,65 @@ const multiplicationTable = function (maxValue) {
 console.log(multiplicationTable(1));
 console.log(multiplicationTable(5));
 console.log(multiplicationTable(10));
+
+// K11
+const PI = 3.14159;
+
+const sphereVolume = function (radius) {
+  let vol = (4 / 3) * PI * Math.pow(radius, 3);
+  return vol;
+};
+
+console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189);
+
+const coneVolume = function (radius, height) {
+  // And here!
+
+  let vol = PI * Math.pow(radius, 2) * (height / 3);
+  return vol;
+};
+
+console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49);
+
+const prismVolume = function (height, width, depth) {
+  let vol = depth * height * width;
+  return vol;
+};
+
+console.log(prismVolume(3, 4, 5) === 60);
+
+let totalVolume = function (solids) {
+  let totalVolume = 0;
+
+  for (let solid of solids) {
+    if (solid.type === "sphere") {
+      totalVolume += sphereVolume(solid.radius);
+    } else if (solid.type === "cone") {
+      totalVolume += coneVolume(solid.radius, solid.height);
+    } else if (solid.type === "prism") {
+      totalVolume += prismVolume(solid.height, solid.width, solid.depth);
+    }
+    console.log(totalVolume);
+  }
+  return totalVolume;
+};
+
+const largeSphere = {
+  type: "sphere",
+  radius: 40,
+};
+
+const smallSphere = {
+  type: "sphere",
+  radius: 10,
+};
+
+const cone = {
+  type: "cone",
+  radius: 3,
+  height: 5,
+};
+
+const duck = [largeSphere, smallSphere, cone];
+
+console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000);
